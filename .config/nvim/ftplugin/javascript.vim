31c469079f077bc2115887554ef0dbaad637a88d
yuzu-eva <stevenhu@web.de>
2022-12-11 22:14:42 +0100

    init desktop branch

--- .config/nvim/ftplugin/javascript.vim
+++
@@ -1,9 +0,0 @@
-setl nospell
-setl nowrap
-setl tabstop=2
-setl shiftwidth=2
-setl expandtab
-let b:vcm_tab_complete = 'javascript'
-let b:javascript_fold = 1
-let g:javascript_plugin_jsdoc = 1
-let g:jsx_ext_required = 0
--- .config/nvim/lua/user/plugins/luasnip.lua
+++
@@ -1,9 +0,0 @@
-local ls = require "luasnip"
-require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
-
-ls.config.set_config {
-    history = true,
-    updateevents = 'TextChanged,TextChangedI',
-    enable_autosnippets = true,
-}
-
