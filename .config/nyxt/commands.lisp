1006987466e98b661a98164c1795ff5682d4260b
yuzu-eva <stevenhu@web.de>
2023-07-01 22:24:19 +0200

    update

--- .config/nyxt/commands.lisp
+++
@@ -1,9 +0,0 @@
-(in-package #:nyxt-user)
-
-(define-command-global show-weather ()
-  (echo (uiop:run-program "~/.config/nyxt/scripts/weather" :output :string)))
-
-;;opens current url in different browser replace firefox with your browser/path
-(define-command-global open-external-browser ()
-  "Open the current url in external browser"
-  (uiop:run-program (list "firefox-bin" (render-url (url (current-buffer))))))
